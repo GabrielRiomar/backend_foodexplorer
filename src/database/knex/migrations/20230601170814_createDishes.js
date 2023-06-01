b121d7758ae95c8c52b2b3c42f97b2b16456894a
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-06-01 14:09:04 -0300

    adding query builder knex to start migrations

---
+++ src/database/knex/migrations/20230601170814_createDishes.js
@@ -0,0 +1,13 @@
+exports.up = knex =>
+  knex.schema.createTable('dishes', table => {
+    table.increments('id')
+    table.text('name').notNullable()
+    table.text('category')
+    table.decimal('price', 14, 2).notNullable()
+    table.text('description')
+    table.text('preview_img').nullable()
+    table.timestamp('created_at').default(knex.fn.now())
+    table.timestamp('updated_at').default(knex.fn.now())
+  })
+
+exports.down = knex => knex.schema.dropTable('users')

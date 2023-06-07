869e0f79aea7a55a973a5610bf33b0e7ac6bb952
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-06-07 13:11:58 -0300

    middleware added

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,9 +1,15 @@
+//Import express Router
 const { Router } = require('express')
+//Import controllers
 const UsersController = require('../controllers/UsersController')
+//Import middlewares
+const ensureAuthenticated = require('../middlewares/ensureAuthenticated')
+//Creating instance for Users
 const usersRoutes = Router()
 const usersController = new UsersController()
 
+//User routes
 usersRoutes.post('/', usersController.create)
-usersRoutes.put('/', usersController.update)
+usersRoutes.put('/', ensureAuthenticated, usersController.update)
 
 module.exports = usersRoutes

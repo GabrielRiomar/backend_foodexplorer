95c152482ed28af019ba312dd6c4717f28779cb5
Gabriel Riomar Lopes <gabrielriomar@gmail.com>
2023-06-07 13:13:24 -0300

    sessions and ingredients routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,15 @@ const { Router } = require('express')
 const usersRoutes = require('./users.routes.js')
 const dishesRoutes = require('./dishes.routes.js')
 const dishesManagerRoutes = require('./dishesManager.routes.js')
+const ingredientsRoutes = require('./ingredients.routes')
+const sessionsRoutes = require('./sessions.routes')
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/dishes', dishesRoutes)
 routes.use('/management', dishesManagerRoutes)
+routes.use('/ingredients', ingredientsRoutes)
+routes.use('/sessions', sessionsRoutes)
 
 module.exports = routes
